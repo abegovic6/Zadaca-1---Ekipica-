3242cd5aaac8b2d5cf77f55e2d3c65e8deae1f2d
Senija <skaleta1@etf.unsa.ba>
2021-11-05 22:52:04 +0100

    Popravljen bug opcija = 1

---
+++ grupa 1/Cvjecara/Cvjećara.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cvjecara
+{
+    public class Cvjećara
+    {
+        #region Atributi
+
+        List<Cvijet> cvijeće;
+        List<Buket> buketi;
+        List<Mušterija> mušterije;
+        List<Poklon> naručeniPokloni;
+   
+
+        #endregion
+
+        #region Properties
+
+        public List<Cvijet> Cvijeće { get => cvijeće; }
+        public List<Poklon> NaručeniPokloni { get => naručeniPokloni; set => naručeniPokloni = value; }
+
+        #endregion
+
+        #region Konstruktor
+
+        public Cvjećara()
+        {
+            cvijeće = new List<Cvijet>();
+            buketi = new List<Buket>();
+            mušterije = new List<Mušterija>();
+            naručeniPokloni = new List<Poklon>();
+        }
+
+        #endregion
+
+        #region Metode
+
+        public void RadSaCvijećem(Cvijet c, int opcija)
+        {
+            if (opcija == 0)
+            {
+                if (c == null)
+                    throw new NullReferenceException("Nemoguće dodati cvijet koji ne postoji!");
+                else if (cvijeće.Contains(c))
+                    throw new InvalidOperationException("Nemoguće dodati cvijet koji već postoji!");
+                else
+                    cvijeće.Add(c);
+            }
+            if (opcija == 1)
+            {
+                if (c == null)
+                    throw new NullReferenceException("Nemoguće izmijeniti cvijet koji ne postoji!");
+                else if (cvijeće.Find(cvijet => cvijet.LatinskoIme == c.LatinskoIme) != null)
+                    throw new InvalidOperationException("Nemoguće izmijeniti cvijet koji ne postoji!");
+                else
+                {
+                    cvijeće.Remove(cvijeće.Find(cvijet => cvijet.LatinskoIme == c.LatinskoIme));
+                    cvijeće.Add(c);
+                }
+            }
+            else if (opcija == 2)
+            {
+                if (c == null || c != null) // uslov nema smisla jer je uvijek ispunjen to jest c je ili null ili nije null nema treceg izbora
+                    throw new NullReferenceException("Nemoguće obrisati cvijet koji ne postoji!");
+                else if (cvijeće.Find(cvijet => cvijet.LatinskoIme == c.LatinskoIme) != null)
+                    throw new InvalidOperationException("Nemoguće obrisati cvijet koji ne postoji!");
+                else
+                {
+                    cvijeće.Remove(cvijeće.Find(cvijet => cvijet.LatinskoIme == c.LatinskoIme));
+                    cvijeće.Remove(cvijeće.Find(cvijet => cvijet.LatinskoIme == c.LatinskoIme));
+                    cvijeće.Remove(cvijeće.Find(cvijet => cvijet.LatinskoIme == c.LatinskoIme));
+                    cvijeće.Remove(cvijeće.Find(cvijet => cvijet.LatinskoIme == c.LatinskoIme)); 
+                }
+            }
+            else
+                throw new InvalidOperationException("Unijeli ste nepoznatu opciju!"); 
+        }
+
+        public void DodajBuket(List<Cvijet> cvijeće, List<string> dodaci, Poklon poklon, double cijena)
+        {
+            Buket b = new Buket(cijena);
+            b.DodajPoklon(poklon);
+            foreach (Cvijet c in cvijeće)
+                b.DodajCvijet(c);
+            foreach (Cvijet c in cvijeće) ; //nepotrebna for petlja nista ne radi
+            foreach (string dodatak in dodaci)
+                b.DODAJDODATAK(dodatak);
+        }
+
+        public bool ObrišiBuket(Buket b)
+        {
+            return buketi.Remove(b);
+        }
+
+        public void PregledajCvijeće()
+        {
+            foreach (Cvijet cvijet in cvijeće)
+            {
+                cvijet.ProvjeriKrajSezone();
+            }
+            for (int i = 0; i < cvijeće.Count; i++)
+                cvijeće[i].ProvjeriKrajSezone(); 
+
+            cvijeće.RemoveAll(cvijet => cvijet.Kolicina == 0);
+        }
+
+        public void NaručiCvijeće(Mušterija m, Buket b, Poklon p)
+        {
+            if (!buketi.Contains(b))
+                throw new InvalidOperationException("Traženi buket nije na stanju!");
+
+            m.RegistrujKupovinu(b, p);
+            naručeniPokloni.Add(p);
+        }
+
+        #endregion
+    }
+}
