80110b2b29e8145a348dfe7906db8d06f90cbf5b
Amila Begović <abegovic6@etf.unsa.ba>
2021-11-18 11:25:25 +0100

    dodane klase za at

---
+++ ZivotinjskaFarma/TestZadatak3/LokacijaTest.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TestZadatak3
+{
+    public class LokacijaTest
+    {
+    }
+}
---
+++ ZivotinjskaFarma/TestZadatak3/ZivotinjaTest.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TestZadatak3
+{
+    [TestClass]
+    public class ZivotinjaTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
