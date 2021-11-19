a70c09e0eac78e0fc040dcbc4c9510646d1b3510
Senija <skaleta1@etf.unsa.ba>
2021-11-19 14:35:53 +0100

    ispravljeno na TestInitialize radi preglednosti i funkcionalnosti

--- ZivotinjskaFarma/TestZadatak3/ProizvodTest.cs
+++ ZivotinjskaFarma/TestZadatak3/ProizvodTest.cs
@@ -8,41 +8,45 @@ namespace TestZadatak3
     [TestClass]
     public class ProizvodTest
     {
+        static Lokacija prebivaliste;
+        static Zivotinja proizvodjac;
+        static Proizvod proizvod;
+
+        [TestInitialize]
+        public void Inizijaliziraj()
+        {
+            prebivaliste = new Lokacija(new List<string>()
+            { "Lokacija", "Ulica", "2", "Sarajevo", "71000", "Bosna i Hercegovina" }, 100);
+            proizvodjac = new Zivotinja(ZivotinjskaVrsta.Koza, DateTime.Now.AddDays(-365), 10, 1.5, prebivaliste);
+            proizvod = new Proizvod("Mlijeko", "Svjeze mlijeko", "Mlijeko", proizvodjac, DateTime.Now, DateTime.Now.AddDays(7), 1);
+
+        }
         [TestMethod]
-        public void TestVrstaSetter1()
+        public void TestSetterVrsta1()
         {
             //test osnovnih postavki
-            Lokacija prebivaliste = new Lokacija(new List<string>()
-            { "Lokacija", "Ulica", "2", "Sarajevo", "71000", "Bosna i Hercegovina" }, 100);
-            Zivotinja proizvodjac = new Zivotinja(ZivotinjskaVrsta.Koza, DateTime.Now.AddDays(-365), 10, 1.5, prebivaliste);
-            Proizvod p = new Proizvod("Mlijeko", "Svjeze mlijeko", "Mlijeko", proizvodjac, DateTime.Now, DateTime.Now.AddDays(7), 1);
-            Assert.AreEqual(p.Proizvođač.Vrsta, ZivotinjskaVrsta.Koza);
-            Assert.AreEqual(p.Vrsta, "Mlijeko");
-            Assert.IsTrue(p.DatumProizvodnje.Date == DateTime.Parse("11/19/2021")); // ovo provjeriti neki drugi dan vidjeti hoce li raditi
-
+            Assert.AreEqual(proizvod.Proizvođač.Vrsta, ZivotinjskaVrsta.Koza);
+            Assert.AreEqual(proizvod.Vrsta, "Mlijeko");
+            Assert.IsTrue(proizvod.DatumProizvodnje.Date == DateTime.Parse("11/19/2021")); // ovo provjeriti neki drugi dan vidjeti hoce li raditi
+            Assert.ThrowsException<InvalidOperationException>(() => proizvod.Vrsta="Meso", "Unijeli ste vrstu proizvoda koja ne postoji!");
         }
 
         [TestMethod]
-        [ExpectedException(typeof(InvalidOperationException))]
-        public void TestVrstaSetter2()
+       // [ExpectedException(typeof(InvalidOperationException))]
+        public void TestSetterProizvodjac()
         {
-            Lokacija prebivaliste = new Lokacija(new List<string>()
-            { "Lokacija", "Ulica", "2", "Sarajevo", "71000", "Bosna i Hercegovina" }, 100);
-            Zivotinja proizvodjac = new Zivotinja(ZivotinjskaVrsta.Koza, DateTime.Now.AddDays(-365), 10, 1.5, prebivaliste);
-            Proizvod p = new Proizvod("Mlijeko", "Svjeze mlijeko", "Piće", proizvodjac, DateTime.Now, DateTime.Now.AddDays(7), 1);
+            Zivotinja pomocna = new Zivotinja(ZivotinjskaVrsta.Krava, DateTime.Now.AddDays(-365), 10, 1.5, prebivaliste);
+            proizvod.Proizvođač = pomocna;
+            Assert.IsTrue(proizvod.Proizvođač.Vrsta == ZivotinjskaVrsta.Krava);
+            pomocna.Vrsta = ZivotinjskaVrsta.Ovca;
+            Assert.IsFalse(proizvod.Proizvođač.Vrsta == ZivotinjskaVrsta.Krava);
+            pomocna.Vrsta = ZivotinjskaVrsta.Magarac;
+            Assert.ThrowsException<InvalidOperationException>(() => { pomocna.Vrsta = ZivotinjskaVrsta.Patka;
+                proizvod.Proizvođač = pomocna;
+            }, "Odabrana životinja ne može proizvoditi željenu vrstu proizvoda!");
+
+
         }
-
-        [TestMethod]
-        public void TestVrstaSetter3()
-        {
-            Lokacija prebivaliste = new Lokacija(new List<string>()
-            { "Lokacija", "Ulica", "2", "Sarajevo", "71000", "Bosna i Hercegovina" }, 100);
-            Zivotinja proizvodjac = new Zivotinja(ZivotinjskaVrsta.Koza, DateTime.Now.AddDays(-365), 10, 1.5, prebivaliste);
-            Assert.ThrowsException<InvalidOperationException>(() => new Proizvod("Mlijeko", "Svjeze mlijeko", "Meso", proizvodjac, DateTime.Now, DateTime.Now.AddDays(7), 1), "Unijeli ste vrstu proizvoda koja ne postoji!");
-        }
-
-
-
 
     }
 }
