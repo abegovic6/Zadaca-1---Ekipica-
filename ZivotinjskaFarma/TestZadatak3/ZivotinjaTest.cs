7781be5faf4f7374a29c772d39e1101dc716edbc
epanjeta <epanjeta1@etf.usna.ba>
2021-11-20 06:10:10 +0100

    neki testovi za klasu zivotinja

--- ZivotinjskaFarma/TestZadatak3/ZivotinjaTest.cs
+++ ZivotinjskaFarma/TestZadatak3/ZivotinjaTest.cs
@@ -8,8 +8,48 @@ namespace TestZadatak3
     [TestClass]
     public class ZivotinjaTest
     {
+
+        static Lokacija lokacija;
+        static Zivotinja zivotinja;
+
+        [TestInitialize]
+        public void Inizijaliziraj()
+        {
+            DateTime dt = new DateTime(2000, 1, 1);
+            lokacija = new Lokacija(new List<string>()
+            { "Lokacija", "Zmaja od Bosne", "2", "Sarajevo", "71000", "Bosna i Hercegovina" }, 1000);
+            zivotinja = new Zivotinja(ZivotinjskaVrsta.Krava, dt, 500, 2, lokacija);
+        }
+
         [TestMethod]
-        public void ProvjeriStanjeZivotinjeTest1() //Eldar Panjeta (18711)
+        public void TestSetterStarost()
+        {
+            Assert.AreEqual(new DateTime(2000, 1, 1), zivotinja.Starost);
+            zivotinja.Starost = new DateTime(2020, 1, 1);
+            Assert.AreEqual(new DateTime(2020, 1, 1), zivotinja.Starost);
+            Assert.ThrowsException<FormatException>(() => zivotinja.Starost = DateTime.Now.AddDays(10), "Životinja ne može biti rođena u budućnosti!");
+        }
+
+        [TestMethod]
+        public void TestSetterTjelesnaMasa()
+        {
+            Assert.AreEqual(500, zivotinja.TjelesnaMasa);
+            zivotinja.TjelesnaMasa = 100;
+            Assert.AreEqual(100, zivotinja.TjelesnaMasa);
+            Assert.ThrowsException<FormatException>(() => zivotinja.TjelesnaMasa = 0.01, "Tjelesna masa ne može biti manja od 0.1 kg!");
+        }
+
+        [TestMethod]
+        public void TestSetterVisina()
+        {
+            Assert.AreEqual(2, zivotinja.Visina);
+            zivotinja.Visina = 1;
+            Assert.AreEqual(1, zivotinja.Visina);
+            Assert.ThrowsException<FormatException>(() => zivotinja.Visina = 0.9, "Visina ne može biti manja od 1 cm!");
+        }
+
+        [TestMethod]
+        public void ProvjeriStanjeZivotinjeTest1() 
         {
             DateTime dt = new DateTime(2000, 1, 1);
             Lokacija l = new Lokacija(new List<string>()
@@ -19,7 +59,7 @@ namespace TestZadatak3
             Assert.IsFalse(z.Proizvođač);
         }
         [TestMethod]
-        public void ProvjeriStanjeZivotinjeTest2() //Eldar Panjeta (18711)
+        public void ProvjeriStanjeZivotinjeTest2() 
         {
             DateTime dt = new DateTime(2020, 1, 1);
             Lokacija l = new Lokacija(new List<string>()
@@ -29,7 +69,7 @@ namespace TestZadatak3
             Assert.IsTrue(z.Proizvođač);
         }
         [TestMethod]
-        public void ProvjeriStanjeZivotinjeTest3() //Eldar Panjeta (18711)
+        public void ProvjeriStanjeZivotinjeTest3() 
         {
             DateTime dt = new DateTime(2013, 1, 1);
             Lokacija l = new Lokacija(new List<string>()
