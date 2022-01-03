b05c4ba89bfca452e03736eed854c41575cd7651
Senija <skaleta1@etf.unsa.ba>
2022-01-03 12:54:55 +0100

    testovi

--- zadaca3/ZivotinjskaFarma/TestniProjekat/UnitTest1.cs
+++ zadaca3/ZivotinjskaFarma/TestniProjekat/UnitTest1.cs
@@ -1,4 +1,5 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
 using ZivotinjskaFarma;
 
 namespace TestniProjekat
@@ -7,9 +8,37 @@ namespace TestniProjekat
     public class UnitTest1
     {
         [TestMethod]
-        public void TestMethod1()
+        public void ObuhvatOdlukaTest1()
         {
             Farma farma = new Farma();
+            Lokacija lokacija = new Lokacija(new System.Collections.Generic.List<string> { "Lokacija", "Ulica", "2", "Sarajevo", "71000", "Bosna i Hercegovina" }, 100);
+            Zivotinja zivotinja = new Zivotinja(ZivotinjskaVrsta.Krava, DateTime.Now.AddDays(-365), 56, 1.56, lokacija);
+            Proizvod proizvod = new Proizvod("Mlijeko", "Mlijeko", "Mlijeko", zivotinja, DateTime.Now, DateTime.Now.AddDays(7), 1);
+            Assert.IsTrue(farma.KupovinaProizvoda(proizvod, DateTime.Now.AddDays(7), 1));
         }
+
+        [TestMethod]
+        public void ObuhvatOdlukaTest2()
+        {
+            Farma farma = new Farma();
+            Lokacija lokacija = new Lokacija(new System.Collections.Generic.List<string> { "Lokacija", "Ulica", "2", "Sarajevo", "71000", "Bosna i Hercegovina" }, 100);
+            Zivotinja zivotinja = new Zivotinja(ZivotinjskaVrsta.Krava, DateTime.Now.AddDays(-365), 56, 1.56, lokacija);
+            Proizvod proizvod = new Proizvod("Mlijeko", "Mlijeko", "Mlijeko", zivotinja, DateTime.Now, DateTime.Now.AddDays(7), 51);
+      
+            Assert.IsFalse(farma.KupovinaProizvoda(proizvod, DateTime.Now.AddDays(7), 51));
+        }
+
+        [TestMethod]
+        public void ObuhvatOdlukaTest3()
+        {
+            Farma farma = new Farma();
+            Lokacija lokacija = new Lokacija(new System.Collections.Generic.List<string> { "Lokacija", "Ulica", "2", "Sarajevo", "71000", "Bosna i Hercegovina" }, 100);
+            Zivotinja zivotinja = new Zivotinja(ZivotinjskaVrsta.Krava, DateTime.Now.AddDays(-365), 56, 1.56, lokacija);
+            Proizvod proizvod = new Proizvod("Mlijeko", "Mlijeko", "Mlijeko", zivotinja, DateTime.Now, DateTime.Now.AddDays(7), 51);
+            farma.KupovinaProizvoda(proizvod, DateTime.Now.AddDays(7), 1);
+            Assert.IsTrue(farma.KupovinaProizvoda(proizvod, DateTime.Now.AddDays(7), 1));
+        }
+
+
     }
 }
