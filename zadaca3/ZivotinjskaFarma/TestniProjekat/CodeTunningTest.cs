8506194c91fcd1eb5913b62867eef291d563d7bf
Amila Begović <abegovic6@etf.unsa.ba>
2022-01-07 22:19:51 +0100

    kreiran test

--- zadaca3/ZivotinjskaFarma/TestniProjekat/CodeTunningTest.cs
+++ zadaca3/ZivotinjskaFarma/TestniProjekat/CodeTunningTest.cs
@@ -15,7 +15,7 @@ namespace TestniProjekat
             Lokacija lokacija = new Lokacija(new List<string>()
             { "Lokacija", "Zmaja od Bosne", "2", "Sarajevo", "71000", "Bosna i Hercegovina" }, 1000);
             Zivotinja zivotinja = new Zivotinja(ZivotinjskaVrsta.Ovca, DateTime.Now.AddDays(-1), 5, 50, lokacija);
-            for(int i = 0; i < 10000000; i++)
+            for(int i = 0; i < 20500000; i++)
                         {
                             Zivotinja zivotinja1 = new Zivotinja(ZivotinjskaVrsta.Krava, DateTime.Now.AddDays(-10), 5, 50, lokacija);
                             farma.Zivotinje.Add(zivotinja1);
@@ -31,7 +31,7 @@ namespace TestniProjekat
             
 
 
-            Assert.IsTrue(farma.Zivotinje.Count == 2);
+            Assert.IsTrue(farma.Zivotinje.Count == 20500001);
         }
 
        
